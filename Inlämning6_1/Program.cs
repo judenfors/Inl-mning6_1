7e47f10e895b089660df2e2c9e9e840ed429ad2d
melvinsjostrand <111962656+Dungeater1337@users.noreply.github.com>
2023-02-10 10:53:53 +0100

    Add project files.

---
+++ Inlämning6_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Inlämning6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett tal");
+            int tal1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Skriv in ett till tal");
+            int tal2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Skriv in ett sista tal");
+            int tal3 = int.Parse(Console.ReadLine());
+
+            int summa = Addera(tal1, tal2, tal3);
+            Console.WriteLine(summa);
+
+        }
+        static int Addera(int tal1, int tal2, int tal3)
+        {
+            int summa = tal1 + tal2 + tal3;
+            return summa;
+        }
+
+    }
+}
+
